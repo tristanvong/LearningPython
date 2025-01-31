f1472f9565dade3cfa328da75acd65cb7319c641
tristanvong <146710405+tristanvong@users.noreply.github.com>
2025-01-31 16:29:44 +0100

    finished chapter1

---
+++ chapter1_intro/main.py
@@ -0,0 +1,5 @@
+# comments written using hashtags
+# printing to console with "print" method
+# string can either be written in single or double quotes
+print("Hello, World!")
+print('Hello, World2!')
