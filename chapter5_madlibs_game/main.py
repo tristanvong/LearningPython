7ebbc9c84ed358d7dffdd0987a637c6c3040a474
tristanvong <146710405+tristanvong@users.noreply.github.com>
2025-01-31 18:06:39 +0100

    finished chapter5

---
+++ chapter5_madlibs_game/main.py
@@ -0,0 +1,13 @@
+# madlibs game => story created by random words
+
+adjective1 = input("Enter an adjective (description): ")
+adjective2 = input("Enter an adjective: ")
+adjective3 = input("Enter an adjective: ")
+noun1 = input("Enter a noun (name): ")
+verb1 = input("Enter a verb (gerund / -ing): ")
+
+print()
+print(f"Last year I went to a {adjective1} museum")
+print(f"I saw the art of the famous painter {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
