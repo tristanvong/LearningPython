62db9598c141d3394f62a756677439e593df1caa
tristanvong <146710405+tristanvong@users.noreply.github.com>
2025-01-31 23:55:54 +0100

    finished chapter12

---
+++ chapter12_conditional_operator/main.py
@@ -0,0 +1,12 @@
+number = 5
+a = 6
+b = 7
+age = 19
+
+print("even" if number % 2 == 0 else "odd")
+max_number = a if a > b else b
+min_number = a if a < b else b
+is_adult = True if age >= 18 else False
+print(max_number)
+print(min_number)
+print(is_adult)
