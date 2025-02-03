42adc737827486e6abd516842adec60b53d37fb9
tristanvong <146710405+tristanvong@users.noreply.github.com>
2025-02-03 15:14:00 +0100

    Finished chapter 22

---
+++ chapter22_shopping_cart/main.py
@@ -0,0 +1,26 @@
+foods = []
+prices = []
+total = 0
+
+while True:
+    food = input("Enter a food to buy (q to quit): ")
+    if food.lower() == "q":
+        break
+    else:
+        price = float(input(f"Enter the price of {food}: "))
+        foods.append(food)
+        prices.append(price)
+
+total = 0
+for price in prices:
+    total += price
+
+products = ""
+for food in foods:
+    if foods.index(food) != len(foods)-1:
+        products += food + ", "
+    else:
+        products += food
+
+print(f"You are buying the following products: {products}")
+print(f"Your total is {total}")
