da844e0f7061d9a73dc3b886b9cba2f12787f542
tristanvong <146710405+tristanvong@users.noreply.github.com>
2025-02-05 00:05:00 +0100

    finished chapter 25

---
+++ chapter25_dictionaries/main.py
@@ -0,0 +1,27 @@
+capitals = {
+    "Belgium":"Brussels",
+    "France": "Paris",
+    "India": "New Delhi",
+    "China": "Beijing"
+}
+
+# print(capitals.get("Belgium"))
+# print(capitals.get("USA")) # Returns "none"
+
+# if capitals.get("France"):
+#     print("This capital exists")
+# else:
+#     print("This capital doesn't exist")
+
+# capitals.update({"Germany":"Berlin"}) # change or add new key/value pair
+# capitals.pop("India") # remove any key/value pair using key
+# capitals.popitem() #remove last item in dictionary
+# capitals.clear()
+# keys = capitals.keys()
+# for key in capitals.keys():
+#     print(key)
+# values = capitals.values()
+# for value in capitals.values():
+#     print(value)
+# for key, value in capitals.items():
+#     print(f"{key}: {value}")
