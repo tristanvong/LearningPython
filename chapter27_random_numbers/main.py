29ed2d02967ddc254ec7d10c6271cf25e5d4f01e
tristanvong <146710405+tristanvong@users.noreply.github.com>
2025-02-05 19:58:59 +0100

    Finished chapter 27

---
+++ chapter27_random_numbers/main.py
@@ -0,0 +1,18 @@
+import random
+# Random integer
+# min_num = 1
+# max_num = 100
+# random_num = random.randint(min_num, max_num)
+# print(random_num)
+
+# random floating point
+# number = random.random() # returns float between 0 and 1
+# print(number)
+
+# options = ("rock", "paper", "scissors")
+# random_choice = random.choice(options)
+# print(random_choice)
+
+# cards = ["2", "3" "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"]
+# random.shuffle(cards)
+# print(cards)
