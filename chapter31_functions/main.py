a67493be3e4e2cdf32f47aa38a7421a161890538
tristanvong <146710405+tristanvong@users.noreply.github.com>
2025-02-05 23:50:12 +0100

    Finished chapter 31

---
+++ chapter31_functions/main.py
@@ -0,0 +1,12 @@
+def happy_birthday(name):
+    print(f"Happy birthday to {name}")
+    print(f"Happy birthday to {name}")
+    print("hip hip hoooray")
+
+happy_birthday("tristan")
+
+def addition(a,b):
+    c = a + b
+    return c
+
+print(addition(3,9))
