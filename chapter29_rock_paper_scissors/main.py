c128c0dbb91e2a30e415a72f0058c495da3a3116
tristanvong <146710405+tristanvong@users.noreply.github.com>
2025-02-05 21:18:10 +0100

    Finished chapter 29

---
+++ chapter29_rock_paper_scissors/main.py
@@ -0,0 +1,32 @@
+import random
+
+options = ("rock", "paper", "scissors")
+player = None
+computer = random.choice(options)
+running = True
+while running:
+    while player not in options:
+        player = input("Enter a choice (rock, paper, scissors): ")
+    print(f"Player: {player}")
+    print(f"Computer: {computer}")
+
+    if player == computer:
+        print("It's a tie")
+    elif player == "rock" and computer == "scissors":
+        print("You win")
+    elif player == "paper" and computer == "rock":
+        print("You win")
+    elif player == "scissors" and computer == "paper":
+        print("You win")
+    else:
+        print("You lose")
+
+    play_status = input("Play again? (Y/N)").upper()
+    if play_status == "Y":
+        pass
+    elif play_status == "N":
+        running = False
+    else:
+        print("unknown input, closing program")
+        running = False
+    player = None
