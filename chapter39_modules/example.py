99305f614830fc4592f8c3257b008c31e69fbcd7
tristanvong <146710405+tristanvong@users.noreply.github.com>
2025-02-07 18:47:32 +0100

    Finished chapter 38

---
+++ chapter39_modules/example.py
@@ -0,0 +1,14 @@
+pi = 3.14159
+
+def square(x):
+    return x ** 2
+
+def cube(x):
+    return x ** 3
+
+def circumference(radius):
+    return 2 * pi * radius
+
+def get_author():
+    return "Tristan Vong"
+
